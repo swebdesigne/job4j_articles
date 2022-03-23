fe30649f341e5009742996c5a4a878210726b8f7
Igor <swebdesigne@gmail.com>
2022-03-23 14:42:36 +0300

    Принципы Kiss, Dry и Yagni

--- test/ru/job4j/kiss/MaxMinTest.java
+++ test/ru/job4j/kiss/MaxMinTest.java
@@ -10,25 +10,25 @@ public class MaxMinTest {
 
     @Test
     public void maxNumber() {
-        int result = new MaxMin().max(List.of(1, 2, 3), (first, second) -> Integer.compare(first, second));
+        int result = new MaxMin().max(List.of(1, 2, 3), Integer::compareTo);
         assertTrue(result == 3);
     }
 
     @Test
     public void minNumber() {
-        int result = new MaxMin().min(List.of(1, 2, 3), (first, second) -> Integer.compare(first, second));
+        int result = new MaxMin().min(List.of(1, 2, 3), Integer::compareTo);
         assertTrue(result == 1);
     }
 
     @Test
     public void maxString() {
-        String result = new MaxMin().max(List.of("Alina", "Igor", "Boris"), (first, second) -> first.compareTo(second));
+        String result = new MaxMin().max(List.of("Alina", "Igor", "Boris"), String::compareTo);
         assertTrue(result.equals("Boris"));
     }
 
     @Test
     public void minString() {
-        String result = new MaxMin().min(List.of("Alina", "Igor", "Boris"), (first, second) -> first.compareTo(second));
+        String result = new MaxMin().min(List.of("Alina", "Igor", "Boris"), String::compareTo);
         assertTrue(result.equals("Alina"));
     }
 }
