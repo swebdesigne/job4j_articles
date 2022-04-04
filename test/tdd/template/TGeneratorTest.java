635f5a59a23523980decfc5fa574b1bd1a01ba68
Igor <swebdesigne@gmail.com>
2022-04-04 10:28:45 +0300

    Шаблонизатор

--- test/tdd/template/TGeneratorTest.java
+++ test/tdd/template/TGeneratorTest.java
@@ -26,5 +26,14 @@ public class TGeneratorTest {
         tGenerator.produce(template, keys);
     }
 
-
+    @Ignore
+    @Test
+    public void whenTemplateHasKeyWhichMapHasToo() {
+        TGenerator tGenerator = new TGenerator();
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> keys = Map.of("name", "Petr", "subject", "Arsentev");
+        String result = tGenerator.produce(template, keys);
+        String expected = "I am a Petr, Who are Arsentev?";
+        assertThat(result, is(expected));
+    }
 }
