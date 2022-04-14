92cb69baaf663fabf67d46a1328a0c1c3c40e4b0
Igor <swebdesigne@gmail.com>
2022-04-14 11:46:16 +0300

    Отчеты

--- src/main/java/ood/srp/designe/srp/TypeSalary.java
+++ src/main/java/ood/srp/designe/srp/TypeSalary.java
@@ -4,20 +4,20 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class TypeSalary implements EstablishSalary {
-    private final double SALARY;
+    private final double salary;
 
     public TypeSalary(double salary) {
-        this.SALARY = salary;
+        this.salary = salary;
     }
 
     @Override
     public double salaryWithPremium(int percent) {
-        return SALARY + (SALARY * percent) / 100;
+        return salary + (salary * percent) / 100;
     }
 
     @Override
     public double salaryWithDeduction(int percent) {
-        return new BigDecimal(SALARY - (SALARY * percent) / 100)
+        return new BigDecimal(salary - (salary * percent) / 100)
                 .setScale(2, RoundingMode.HALF_EVEN).doubleValue();
     }
 }
