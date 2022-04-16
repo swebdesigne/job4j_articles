a7577b796829f78decf5ce57918803aca8ea1d25
swebdesigne <swebdesigne@gmail.com>
2022-04-16 17:30:35 +0300

    Принцип открытости закрытости

---
+++ src/main/java/ood/srp/designe/srp/osp/product/Logger.java
@@ -0,0 +1,11 @@
+package ood.srp.designe.srp.osp.product;
+
+public class Logger {
+    private void saveToFile(String message) {
+
+    }
+
+    public void log(String message) {
+        saveToFile(message);
+    }
+}
