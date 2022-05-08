abb07b68c02b1c6f032875b3be6a67b4319b629c
swebdesigne <swebdesigne@gmail.com>
2022-05-08 20:17:29 +0300

    Хранилище продуктов

--- src/main/java/ood/lsp/food/products/Ananas.java
+++
@@ -1,9 +0,0 @@
-package ood.lsp.food.products;
-
-import java.time.LocalDate;
-
-public class Ananas extends Food {
-    public Ananas(String name, double price, int discount, String category, LocalDate createDate, LocalDate expireDate) {
-        super(name, price, discount, category, createDate, expireDate);
-    }
-}
--- src/main/java/ood/lsp/food/products/Banana.java
+++
@@ -1,9 +0,0 @@
-package ood.lsp.food.products;
-
-import java.time.LocalDate;
-
-public class Banana extends Food {
-    public Banana(String name, double price, int discount, String category, LocalDate createDate, LocalDate expireDate) {
-        super(name, price, discount, category, createDate, expireDate);
-    }
-}
