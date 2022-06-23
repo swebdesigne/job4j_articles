b4272b3282504532b7c818baa1e9915274b82d31
Igor <swebdesigne@gmail.com>
2022-06-23 17:50:08 +0300

    Парковка машин

--- src/main/java/ood/lsp/parking/CargoCar.java
+++ src/main/java/ood/lsp/parking/CargoCar.java
@@ -1,42 +1,10 @@
 package ood.lsp.parking;
 
-import java.util.Objects;
-
-public class CargoCar implements IVehicle {
-     private final int sizeParkingPlace;
-     private final String number;
-
+public class CargoCar extends AbstractVehicle {
      public CargoCar(int sizeParkingPlace, String number) {
+          super(sizeParkingPlace, number);
           if (sizeParkingPlace <= PassengerCar.SIZE) {
                throw new IllegalArgumentException("Size of the parking place should be more than `SIZE`");
           }
-          this.sizeParkingPlace = sizeParkingPlace;
-          this.number = number;
-     }
-
-     @Override
-     public int getSize() {
-          return sizeParkingPlace;
-     }
-
-     public String getNumber() {
-          return number;
-     }
-
-     @Override
-     public boolean equals(Object o) {
-          if (this == o) {
-               return true;
-          }
-          if (o == null || getClass() != o.getClass()) {
-               return false;
-          }
-          CargoCar car = (CargoCar) o;
-          return sizeParkingPlace == car.sizeParkingPlace && Objects.equals(number, car.number);
-     }
-
-     @Override
-     public int hashCode() {
-          return Objects.hash(sizeParkingPlace, number);
      }
 }
--- src/main/java/ood/lsp/parking/PassengerCar.java
+++ src/main/java/ood/lsp/parking/PassengerCar.java
@@ -1,40 +1,9 @@
 package ood.lsp.parking;
 
-import java.util.Objects;
-
-public class PassengerCar implements IVehicle {
+public class PassengerCar extends AbstractVehicle {
     public static final int SIZE = 1;
-    private final int sizeParkingPlace;
-    private final String number;
 
     public PassengerCar(String number) {
-        this.sizeParkingPlace = SIZE;
-        this.number = number;
-    }
-
-    @Override
-    public int getSize() {
-        return sizeParkingPlace;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        PassengerCar that = (PassengerCar) o;
-        return sizeParkingPlace == that.sizeParkingPlace && Objects.equals(number, that.number);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(sizeParkingPlace, number);
+        super(SIZE, number);
     }
 }
