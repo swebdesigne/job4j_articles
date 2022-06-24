102a9e5f231aeaa5959b486d6b3ddeea6a931ad7
Igor <swebdesigne@gmail.com>
2022-06-24 12:59:51 +0300

    Парковка машин

--- src/main/java/ood/lsp/parking/Parking.java
+++ src/main/java/ood/lsp/parking/Parking.java
@@ -5,31 +5,27 @@ import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 public class Parking implements IParking {
-    private Place typeParking;
-    private final List<Place> place;
+    private final Place passengerCarsPlaces;
+    private final Place cargoCarsPlaces;
 
     public Parking(int sizePassenger, int sizeCargo) {
-        place = List.of(new Place(sizePassenger), new Place(sizeCargo));
+        passengerCarsPlaces = new Place(sizePassenger);
+        cargoCarsPlaces = new Place(sizeCargo);
     }
 
-    private Place getTypeParking() {
-        return typeParking;
-    }
-
-    private void setTypeParking(int size) {
-        typeParking = (size == PassengerCar.SIZE) ? place.get(0) : place.get(1);
+    private Place getTypeParking(int size) {
+        return (size == PassengerCar.SIZE) ? passengerCarsPlaces : cargoCarsPlaces;
     }
 
     @Override
     public boolean takePlace(IVehicle car) {
         int sizeParking = car.getSize();
-        setTypeParking(sizeParking);
-        OptionalInt optional = availablePlaceOnParking();
+        Place parking = getTypeParking(sizeParking);
+        OptionalInt optional = availablePlaceOnParking(sizeParking);
         if (optional.isPresent() && !checkCarExists(car)) {
-            getTypeParking().addCar(optional.getAsInt(), car);
+            parking.addCar(optional.getAsInt(), car);
         } else {
             if (sizeParking > PassengerCar.SIZE) {
-                setTypeParking(PassengerCar.SIZE);
                 Optional<String> isExists = pairAvailableParkingPlace(car);
                 if (isExists.isEmpty() || checkCarExists(car)) {
                     return false;
@@ -43,8 +39,8 @@ public class Parking implements IParking {
     }
 
     private boolean checkCarExists(IVehicle car) {
-        for (int i = 0; i < getTypeParking().size(); i++) {
-            Optional<IVehicle> optional = Optional.ofNullable(getTypeParking().getPlace()[i]);
+        for (int i = 0; i < getTypeParking(car.getSize()).size(); i++) {
+            Optional<IVehicle> optional = Optional.ofNullable(getTypeParking(car.getSize()).getPlace()[i]);
             if (optional.isPresent() && optional.get().equals(car)) {
                 return true;
             }
@@ -54,7 +50,7 @@ public class Parking implements IParking {
 
     @Override
     public Optional<String> pairAvailableParkingPlace(IVehicle car) {
-        Place passengerParking = place.get(0);
+        Place passengerParking = getTypeParking(1);
         return IntStream.range(0, passengerParking.size())
                 .filter(index -> index < passengerParking.size() - 1)
                 .filter(index -> checkPairEmptyPlace(index, car.getSize(), passengerParking))
@@ -84,7 +80,7 @@ public class Parking implements IParking {
     private void takePairPlaceForCargoCarInPassengerParking(String pairPlace, IVehicle car) {
         Scanner scanner = new Scanner(pairPlace.replaceAll("[^\\d ]", ""));
         while (scanner.hasNextInt()) {
-            getTypeParking().addCar(scanner.nextInt(), car);
+            getTypeParking(1).addCar(scanner.nextInt(), car);
         }
         scanner.close();
     }
@@ -100,26 +96,27 @@ public class Parking implements IParking {
     }
 
     private Map<Integer, List<Integer>> getAllPlaceByPredicate(Predicate<Boolean> predicate) {
-        Map<Integer, List<Integer>> parking = new HashMap<>();
+        Map<Integer, List<Integer>> places = new HashMap<>();
+        List<Place> place = List.of(passengerCarsPlaces, cargoCarsPlaces);
         for (int i = 0; i < place.size(); i++) {
             List<Integer> value = new ArrayList<>();
             Place tmp = place.get(i);
             for (int j = 0; j < tmp.size(); j++) {
                 if (predicate.test(tmp.getStatus(j))) {
                     value.add(j);
-                    parking.put(i, value);
+                    places.put(i, value);
                 }
             }
         }
-        return parking;
+        return places;
     }
 
-    private OptionalInt availablePlaceOnParking() {
-        return getPlaceByPredicate(getTypeParking()::getStatus);
+    private OptionalInt availablePlaceOnParking(int size) {
+        return getPlaceByPredicate(size, getTypeParking(size)::getStatus);
     }
 
-    private OptionalInt getPlaceByPredicate(Predicate<Integer> predicate) {
-        return IntStream.range(0, getTypeParking().size()).filter(predicate::test).findFirst();
+    private OptionalInt getPlaceByPredicate(int size, Predicate<Integer> predicate) {
+        return IntStream.range(0, getTypeParking(size).size()).filter(predicate::test).findFirst();
     }
 }
 
